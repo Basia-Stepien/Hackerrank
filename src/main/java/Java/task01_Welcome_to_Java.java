03031d2b2fed2573fa02df63f17444e1551f47dd
Basia-Stepien <bases_01@protonmail.com>
2022-07-26 10:24:36 +0200

    create Hackerrank project and task01_Welcome_to_Java

---
+++ src/main/java/Java/task01_Welcome_to_Java.java
@@ -0,0 +1,10 @@
+//https://www.hackerrank.com/challenges/welcome-to-java/problem?isFullScreen=true
+package Java;
+
+public class task01_Welcome_to_Java {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
