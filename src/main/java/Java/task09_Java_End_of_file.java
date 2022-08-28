150f2708bd7a8895bec7f0529fa6027d2a13df11
Basia-Stepien <bases_01@protonmail.com>
2022-08-28 12:32:29 +0200

    solved task09_Java_End_of_file

---
+++ src/main/java/Java/task09_Java_End_of_file.java
@@ -0,0 +1,20 @@
+package Java;
+
+import java.util.*;
+
+public class task09_Java_End_of_file {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        StringBuilder result = new StringBuilder();
+
+        int i =1;
+        do {
+            result.append(i).append(" ").append(in.nextLine()).append("\n");
+            i++;
+        } while(in.hasNext());
+
+        System.out.println(result);
+    }
+
+}
