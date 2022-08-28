963a35ab7d94aa4953b6fa05933da3fa7563f683
Basia-Stepien <bases_01@protonmail.com>
2022-08-28 12:35:06 +0200

    solve task10_Java_Int_to_String

---
+++ src/main/java/Java/task10_Java_Int_to_String.java
@@ -0,0 +1,15 @@
+package Java;
+
+import java.util.*;
+
+public class task10_Java_Int_to_String {
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        String s = String.valueOf(n);
+        if (s.equals(String.valueOf(n))) System.out.println("Good job");
+    }
+
+}
